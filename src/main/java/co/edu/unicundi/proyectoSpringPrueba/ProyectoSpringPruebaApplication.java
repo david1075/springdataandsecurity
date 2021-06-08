9db9ee9ba4fea6a61b435750183b5a15253ca94a
david1075 <jmdavidjimenez@outlook.es>
2021-06-08 16:17:53 -0500

    se configura .jar y despliegue automatico tomcat

--- src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
+++ src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
@@ -2,11 +2,16 @@ package co.edu.unicundi.proyectoSpringPrueba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class ProyectoSpringPruebaApplication {
+	
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		 return application.sources(ProyectoSpringPruebaApplication.class);
+		 }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringPruebaApplication.class, args);
