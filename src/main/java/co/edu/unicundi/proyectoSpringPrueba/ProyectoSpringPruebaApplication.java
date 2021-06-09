3c05fcc33a4591877869faa818332f53310c9813
david1075 <jmdavidjimenez@outlook.es>
2021-06-09 07:54:20 -0500

    configuracion clase main

--- src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
+++ src/main/java/co/edu/unicundi/proyectoSpringPrueba/ProyectoSpringPruebaApplication.java
@@ -3,18 +3,22 @@ package co.edu.unicundi.proyectoSpringPrueba;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class ProyectoSpringPruebaApplication {
+public class ProyectoSpringPruebaApplication extends SpringBootServletInitializer {
 	
-	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		 return application.sources(ProyectoSpringPruebaApplication.class);
-		 }
+	 
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringPruebaApplication.class, args);
 	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		 return application.sources(ProyectoSpringPruebaApplication.class);
+		 }
 
 }
